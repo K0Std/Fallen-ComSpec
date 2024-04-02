bb777191542601bf236983b66e6c83ce995b5659
HOSS <163043189+K0Std@users.noreply.github.com>
2024-04-02 18:55:41 +0200

    Added Command Line Tabs.

--- src/Fallen.ComSpec.cpp
+++ src/Fallen.ComSpec.cpp
@@ -5,10 +5,12 @@
 #include <filesystem>
 #include <vector>
 #include <color.hpp>
-#include "Fallen_COMSPEC.hpp"
+#include "headers/Fallen_COMSPEC.hpp"
 using namespace std;
 void Start(){
     auto Console = conhost::Shell();
+    auto DefaultTab = console_tabs::tab("Default",0,"Default.history");
+    console_tabs::ActiveTab = DefaultTab;
     conhost::Install_DIR = filesystem::current_path().string();
     string Command = "";
     while (true){
@@ -16,44 +18,8 @@ void Start(){
         cout << dye::light_aqua(filesystem::current_path().string() + "\n");
         cout << dye::green("$ ");
         getline(cin,Command);
-        if (Command == ""){
-            continue;
-        }
-        if (strstr(Command.c_str(),"\t") && !(strstr(Command.c_str(),"\\"))){
-            Command.erase(Command.find("\t"),Command.find("\t"));
-            Console.AutoComplete(conhost::ConvertToUppercase(Command));
-            continue;
-        }
-        if (strstr(Command.c_str(),"\t") && strstr(Command.c_str(),"\\")){
-            Console.PathAutoComplete();
-            continue;
-        }else if (strstr(Command.c_str(),"/?")){
-            vector<string> Args;
-            conhost::Split(Command,Args,' ');
-            string CMD_Name = Args[0];
-            Console.Generate_CMD_HELP(CMD_Name);
-            continue;
-        }else if (strstr(conhost::ConvertToUppercase(Command.c_str()).c_str(),"CD ")){
-            string New_Path = Command;
-            New_Path.erase(0,3);
-            filesystem::current_path(New_Path);
-            continue;
-        }else if (strstr(conhost::ConvertToUppercase(Command.c_str()).c_str(),"NETUTILS")){
-            vector<string> NetUtilsARGV;
-            conhost::Split(Command,NetUtilsARGV,' ');
-            Console.NetUtils(NetUtilsARGV);
-            continue;
-        }else if (strstr(conhost::ConvertToUppercase(Command.c_str()).c_str(),"CHDIR ")){
-            string New_Path = Command;
-            New_Path.erase(0,6);
-            filesystem::current_path(New_Path);
-            continue;
-        }else if (Command == "exit"){
-            break;
-        }else{
-            Console.Eval(Command);
-            continue;
-        }
+        Console.DumpHistory(Command,console_tabs::ActiveTab.HistoryFile);
+        Console.Eval(Command);
     };
     };
 int main(int argc,char* argv[]){
